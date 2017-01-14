80f0d7a067e42373bff6b09f66a921655fb1e8ae
Herbertvc <hvidelac@gmailcom>
2017-01-14 12:24:39 -0500

    Added the api to create and show users

---
+++ api/README.md
@@ -0,0 +1,14 @@
+# Fake Users API
+
+## Install
+
+`npm i`
+
+## Run
+
+`npm start`
+
+## API
+
+### `/users/[count]` default count is 10.
+
---
+++ api/index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const bole = require('bole')
+const pretty = require('bistre')()
+const fake = require('fake-json-stream')
+const serverRouter = require('server-router')
+const from = require('from2-string')
+
+bole.output({
+  level: 'info',
+  stream: pretty
+})
+
+const log = bole('server')
+pretty.pipe(process.stdout)
+
+const router = serverRouter({default: '/404'}, [
+  ['/404', (req, res) => from('404: not found')],
+  ['/users', (req, res) => fake(10)],
+  ['/users/:count', (req, res, params) => {
+    return fake(Number(params.count) || 10)
+  }]
+])
+
+http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  log.info(req)
+  router(req, res).pipe(res)
+}).listen(3000, () => {
+  log.info('Listening at http://localhost:3000')
+})
